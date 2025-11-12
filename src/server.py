0e3c16e5a95cf43bc585f1d4abc8eddc461c0067
Tom Robinson <tarobins@gmail.com>
2025-11-11 22:42:15 -0800

    Feat: Add HTTPS support with self-signed certificate

--- src/server.py
+++ src/server.py
@@ -52,4 +52,4 @@ def download_file(filename):
     return send_from_directory(app.config['PROCESSED_FOLDER'], filename, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=5001)
+    app.run(host='0.0.0.0', debug=True, port=5001, ssl_context=('cert.pem', 'key.pem'))
